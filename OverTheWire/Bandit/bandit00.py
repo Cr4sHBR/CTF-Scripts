7bf73db5f25d3c76aa89a2a9917dd95a7b1db661
Cr4sHBR <du.simionato@gmail.com>
2019-02-05 03:18:12 -0200

    Add files via upload

---
+++ OverTheWire/Bandit/bandit00.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from paramiko import SSHClient
+import paramiko
+
+
+server = "bandit.labs.overthewire.org"
+port = "2220"
+username = "bandit0"
+password = "bandit0"
+
+
+class SSH:
+    def __init__(self):
+        self.ssh = SSHClient()
+        self.ssh.load_system_host_keys()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect(hostname=server ,port=port ,username=username,password=password)
+
+    def exec_cmd(self,cmd):
+        stdin,stdout,stderr = self.ssh.exec_command(cmd)
+        if stderr.channel.recv_exit_status() != 0:
+            print stderr.read()
+        else:
+            print "Your Flag is -> "+stdout.read()
+
+
+if __name__ == '__main__':
+    ssh = SSH()
+    ssh.exec_cmd("cat readme")
